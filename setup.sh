8f3f2d32350192b294e5ad8d10178dcb25722c21
Rares Cosma <office@rarescosma.com>
2014-05-17 10:13:23 +0300

    Provision node

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# Ask for root upfront
+sudo -v
+
 # Add node.js PPA and install
 sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install --yes nodejs
 
-which npm
+# Add git
+sudo apt-get install git-core
+
+# Clone the repo
+git clone https://github.com/rarescosma/dotfiles.git .dotfiles
+cd .dotfiles
+
+npm install
