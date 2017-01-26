4e93807f5a94d0619aca48e7951781537c992d8c
Rares Cosma <rares@getbetter.ro>
2017-01-26 18:13:50 +0200

    Libize xrandr scripts

--- _xrandr/lib.sh
+++ _xrandr/lib.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 do_math() {
-	local exp="${1}"
-	echo $exp | bc | awk '{printf("%d\n",$1 + 0.5)}'
+  local exp="${1}"
+  echo $exp | bc | awk '{printf("%d\n",$1 + 0.5)}'
 }
 
 # Internal monitor + its resolution
@@ -15,10 +15,6 @@ IH="1800"
 external="DP1"
 EW="3440"
 EH="1440"
-SCALE="1.25"
+SCALE="1"
 EW_SCALED=$(do_math "${EW}*${SCALE}")
 EH_SCALED=$(do_math "${EH}*${SCALE}")
-
-xrandr --output $external --scale "${SCALE}x${SCALE}" --mode "${EW}x${EH}" \
-	--fb "$(($IW + $EW_SCALED))x${EH_SCALED}" --pos "0x0" \
-	--output $internal --scale "1x1" --pos "${EW_SCALED}x0"
