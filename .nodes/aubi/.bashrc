dc4813442727c2053165edb0348115a44ed26c49
Rares Cosma <rares@getbetter.ro>
2018-01-09 23:47:16 +0100

    Shuffle in the env.nodes repo

---
+++ .nodes/aubi/.bashrc
@@ -0,0 +1,4 @@
+alias l='ls -lah'
+alias sau='sudo apt-get update'
+alias sai='sudo apt-get install'
+
---
+++ .nodes/turris/seedbox/flood/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+  baseURI: '/',
+  dbCleanInterval: 1000 * 60 * 60,
+  dbPath: './server/db/',
+  floodServerPort: 3000,
+  maxHistoryStates: 30,
+  pollInterval: 1000 * 5,
+  secret: 'flood',
+  scgi: {
+    host: '192.168.5.140',
+    port: 5000,
+    socket: false,
+    socketPath: '/tmp/rtorrent.sock'
+  },
+  ssl: false,
+  sslKey: '/absolute/path/to/key/',
+  sslCert: '/absolute/path/to/certificate/'
+};
+
+module.exports = CONFIG;
