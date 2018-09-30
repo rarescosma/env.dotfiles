6252f960d81da29ab675e67c2dc6fe0dce7ba58d
Rares Cosma <rares@getbetter.ro>
2018-09-30 03:14:14 +0300

    Manage autoenv via git submodule

--- shell/.zsh/oh-my-zplug.zsh
+++ shell/.zsh/oh-my-zplug.zsh
@@ -3,4 +3,5 @@ plugins=(gitalias archlinux docker git lxd kubectl zsh-autosuggestions)
 source "$ZSH/oh-my-zsh.sh"
 source "$_VENDOR/z/z.sh"
 source "$_VENDOR/fz/fz.sh"
+source "$_VENDOR/autoenv/autoenv.plugin.zsh"
 
