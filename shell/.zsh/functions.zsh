3765bb8d9e28e64d7a4a27b73a8d23b568ab49da
Rareș Cosma <rares@getbetter.ro>
2020-01-08 16:16:31 +0100

    Reorg dropbox structure

--- shell/.zsh/functions.zsh
+++ shell/.zsh/functions.zsh
@@ -94,7 +94,7 @@ dps() {
 # -- Z / Misc ------------------------------------------------------------------
 alias ccat='pygmentize -g'
 alias pbcopy='xsel -b'
-alias t="t -t ${HOME}/Dropbox/_Tasks"
+alias t="t -t ${HOME}/Dropbox/apps/t"
 alias tlk="t -l k"
 alias tlro="t -l ro"
 alias tnm='tmuxp load -y ~/.tmuxp/misc.yaml'
