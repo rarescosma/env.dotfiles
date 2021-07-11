0df37091646be1e933c8a3f543592a09f0ff10c6
Rareș Cosma <rares@getbetter.ro>
2021-07-11 16:41:42 +0200

    [backup] set up automatic glacier backup of ~/media

---
+++ _arch/etc/backup/autoglacier/autoglacier.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+"${HOME}"/bin/rsync-mirror "${HOME}"/media
+
+export PATH="$PATH:/usr/bin/vendor_perl"
+MTGLACIER="mtglacier sync --config ${HOME}/.aws/$(hostname -s).cfg --dir ${HOME}/media/.rsync_mirror --concurrency 16"
+
+$MTGLACIER --dry-run
+$MTGLACIER
