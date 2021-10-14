a2aa3e715ecd7d31e820dcc96bdb4ac01ad94e97
Rareș Cosma <rares@getbetter.ro>
2021-10-14 23:55:57 +0200

    have some fun with caffeine & melatonin

--- wm/.i3/locker.sh
+++ wm/.i3/locker.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if test -f /tmp/caffeine; then
+  echo "had caffeine, will not lock"
+  exit 0
+fi
+
 if [[ -z "$(pidof i3lock)" ]]; then
   i3lock -i ~/.i3/congruent_outline.png --tiling && sleep 1
 else
