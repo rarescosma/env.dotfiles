763105952a05367274a9af35ffd26da91ec5030b
Rareș Cosma <rares@getbetter.ro>
2021-12-03 13:47:29 +0100

    hib

--- setup-system.sh
+++ setup-system.sh
@@ -75,6 +75,7 @@ setup::_node() {
     copy_host "etc/modprobe.d/i915.conf"
     copy_host "etc/X11/xorg.conf.d/20-synaptics.conf"
     copy_host "etc/environment"
+    copy_host "etc/mkinitcpio.conf"
   fi
 
   if [[ $HOSTNAME == mac ]]; then
