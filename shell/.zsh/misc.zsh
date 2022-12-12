1f7c2884d78b536c82885445f498e8cac836b321
Rareș Cosma <rares@getbetter.ro>
2022-12-12 21:03:09 +0200

    silence old ssh socket removal commands

--- shell/.zsh/misc.zsh
+++ shell/.zsh/misc.zsh
@@ -48,7 +48,7 @@ surroundtutor() {
 
 # -- ssh-agent -----------------------------------------------------------------
 if [ -z "$(pgrep ssh-agent)" ]; then
-   rm -rf /tmp/ssh-*
+   /sbin/rm -rf /tmp/ssh-* >/dev/null
    eval $(ssh-agent -s) > /dev/null
 else
    export SSH_AGENT_PID=$(pgrep ssh-agent)
