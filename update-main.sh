85134fd09dd248b08d2b6a8fade9c387fddc0475
Rareș Cosma <rares@getbetter.ro>
2023-01-30 12:10:17 +0100

    refactor: split update-main script into functions

--- update-main.sh
+++ update-main.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-# move current host customizations as the last commit
-GIT_SEQUENCE_EDITOR=./bin/git-local-last git rebase -i main
+rebase() {
+    # move current host customizations as the last commit
+    GIT_SEQUENCE_EDITOR=./bin/git-local-last git rebase -i main
+}
 
-# move local main one behind
-git branch -f main HEAD~1
+push_main() {
+    # move local main one behind
+    git branch -f main HEAD~1
 
-# update origin
-git push origin main:main "$@"
+    # update origin
+    git push origin main:main $*
+}
+
+if [[ "$@" != "" ]]; then
+    $@
+else
+    rebase
+    push_main
+fi
 
