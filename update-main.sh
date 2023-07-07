47730cdffd0de893d145f2efa37bb4e0bc30ee32
Rareș Cosma <rares@getbetter.ro>
2023-07-07 20:36:44 +0200

    git: ignore arch-packs when advancing main

--- update-main.sh
+++ update-main.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+stash_arch_packs() {
+    git update-index --no-assume-unchanged _arch/package*
+    git stash
+}
+
+unchange_arch_packs() {
+    git update-index --assume-unchanged _arch/package*
+}
+
 rebase() {
+    stash_arch_packs
     # move current host customizations as the last commit
     GIT_SEQUENCE_EDITOR=./bin/git-local-last git rebase -i main
+    unchange_arch_packs
 }
 
 push_main() {
