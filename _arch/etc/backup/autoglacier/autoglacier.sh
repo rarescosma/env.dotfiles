678ab10a3fe84a30dfae1299d7e27541de7f462f
Rareș Cosma <rares@getbetter.ro>
2024-05-06 11:18:18 +0200

    draft: switch from mtglacier to rclone

--- _arch/etc/backup/autoglacier/autoglacier.sh
+++ _arch/etc/backup/autoglacier/autoglacier.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-"${HOME}"/bin/rsync-mirror "${HOME}"/media
+HOST="$(hostname -s)"
 
-export PATH="$PATH:/usr/bin/vendor_perl"
-MTGLACIER="mtglacier sync --config ${HOME}/.config/aws/$(hostname -s).cfg --dir ${HOME}/media/.rsync_mirror --concurrency 16"
-
-$MTGLACIER --dry-run
-$MTGLACIER
+if [[ "$HOST" == "seedbox" ]]; then
+  "${HOME}"/bin/rsync-mirror /mnt/media
+  rclone sync /mnt/media/.rsync_mirror/ turris-backup:turris-backup/media/ \
+    --transfers=4              \  # a little parallelism
+    --size-only                \  # it's media so size-only comparison suffices
+    --links                    \  # create .rclonelink for symlinks
+    --no-update-dir-modtime    \  # don't spam the API for no reason
+    --no-update-modtime        \  # ditto
+    --dry-run                  \  # development mode :-)
+fi
