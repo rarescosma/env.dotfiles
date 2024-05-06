601debf003e0a075be027679fd0ec98dba09f848
Rareș Cosma <rares@getbetter.ro>
2024-05-06 11:18:18 +0200

    backup: switch from mtglacier to rclone

--- _arch/etc/backup/autoglacier/autoglacier.sh
+++ _arch/etc/backup/autoglacier/autoglacier.sh
@@ -3,6 +3,8 @@
 set -e
 
 HOST="$(hostname -s)"
+EXTRA_ARGS="$*"
+test -z "$EXTRA_ARGS" && EXTRA_ARGS="--dry-run"
 
 if [[ "$HOST" == "seedbox" ]]; then
   "${HOME}"/bin/rsync-mirror /mnt/media
@@ -11,9 +13,9 @@ if [[ "$HOST" == "seedbox" ]]; then
  --size-only`             # it's media so size-only comparison suffices`\
  --links`                 # create .rclonelink for symlinks`\
  --no-update-modtime`     # don't spam the API for no reason`\
- --dry-run`               # development mode`
+ $EXTRA_ARGS
 elif [[ "$HOST" == "vps" ]]; then
   rclone sync "${HOME}/backup/" vps-borg-backup:vps-borg-backup/ \
  --transfers=8`           # more parallelism`\
- --dry-run`               # development mode`
+ $EXTRA_ARGS
 fi
