133d0083c76a4ded1a5b844c6da35ec20bc9655a
Rareș Cosma <rares@getbetter.ro>
2024-05-06 11:40:43 +0200

    mtglacier to rclone part 2

--- _arch/etc/backup/autoglacier/autoglacier.sh
+++ _arch/etc/backup/autoglacier/autoglacier.sh
@@ -7,10 +7,13 @@ HOST="$(hostname -s)"
 if [[ "$HOST" == "seedbox" ]]; then
   "${HOME}"/bin/rsync-mirror /mnt/media
   rclone sync /mnt/media/.rsync_mirror/ turris-backup:turris-backup/media/ \
-    --transfers=4              \  # a little parallelism
-    --size-only                \  # it's media so size-only comparison suffices
-    --links                    \  # create .rclonelink for symlinks
-    --no-update-dir-modtime    \  # don't spam the API for no reason
-    --no-update-modtime        \  # ditto
-    --dry-run                  \  # development mode :-)
+ --transfers=4`           # a little parallelism`\
+ --size-only`             # it's media so size-only comparison suffices`\
+ --links`                 # create .rclonelink for symlinks`\
+ --no-update-modtime`     # don't spam the API for no reason`\
+ --dry-run`               # development mode`
+elif [[ "$HOST" == "vps" ]]; then
+  rclone sync "${HOME}/backup/" vps-borg-backup:vps-borg-backup/ \
+ --transfers=8`           # more parallelism`\
+ --dry-run`               # development mode`
 fi
