5541f3de50045706cfb9bbdad1506fb25bdb7bfc
Rareș Cosma <rares@getbetter.ro>
2024-10-05 13:29:53 +0300

    let immutable files mutate during rebase

--- update-main.sh
+++ update-main.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 set -e
 
-stash_arch_packs() {
+IMMUS=""
+
+_stash_arch_packs() {
     git update-index --no-assume-unchanged _arch/package*
     git stash
 }
 
-unchange_arch_packs() {
+_unchange_arch_packs() {
     git update-index --assume-unchanged _arch/package*
 }
 
+_restore_immus() {
+    while IFS= read -r line; do
+        test -n "$line" && sudo chattr +i "$line"
+    done <<< "$IMMUS"
+}
+
+_rebase_pre() {
+    _stash_arch_packs
+    IMMUS="$(lsattr -laR . 2>/dev/null | grep Immutable | sed -e 's#\s*Immutable$##g')"
+    while IFS= read -r line; do
+        test -n "$line" && sudo chattr -i "$line"
+    done <<< "$IMMUS"
+}
+
+_rebase_post() {
+    _unchange_arch_packs
+    _restore_immus
+}
+
 rebase() {
-    stash_arch_packs
+    _rebase_pre
     # move current host customizations as the last commit
-    GIT_SEQUENCE_EDITOR=./bin/git-local-last git rebase -i main
-    unchange_arch_packs
+    GIT_SEQUENCE_EDITOR=./bin/git-local-last git rebase -i main || _restore_immus
+    _rebase_post
 }
 
 push_main() {
