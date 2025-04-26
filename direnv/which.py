8f85f7940fc1ded38ce631be64c158ae7958f270
Rareș Cosma <rares@getbetter.ro>
2025-04-26 20:24:13 +0200

    python-which: let the caller worry about space

--- direnv/which.py
+++ direnv/which.py
@@ -6,4 +6,4 @@ if home is not None and executable.startswith(home):
     executable = executable.replace(home, "~")
 ver = sys.version.replace(" \n", " ")
 
-print(f'\n\033[1m\033[94m>> {executable} \033[93m{ver} <<\033[0m\n')
+print(f'\033[1m\033[94m>> {executable} \033[93m{ver} <<\033[0m\n')
